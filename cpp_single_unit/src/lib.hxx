b4a3bdf4526b32fc743de1314115103a0221d91d
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-08-18 23:22:03 +0300

    Added a new skeleton: C++ single compilation unit project.

---
+++ cpp_single_unit/src/lib.hxx
@@ -0,0 +1,6 @@
+// A library skeleton.
+#ifndef LIB_HXX
+#define LIB_HXX
+
+
+#endif // LIB_HXX
---
+++ cpp_single_unit/src/main.cxx
@@ -0,0 +1,9 @@
+// A skeleton of a C++ single compilation unit project.
+#include <iostream>
+
+#include "lib.hxx"
+
+int main() {
+    std::cout << "Hello, SCU C++!" << std::endl;
+    return 0;
+}
